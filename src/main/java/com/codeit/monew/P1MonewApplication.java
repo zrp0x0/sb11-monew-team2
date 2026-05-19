5e2d1da78e3f63997a2896fc12f65aa76a526ab2
zrp0x0 <zrp0x0@gmail.com>
2026-05-20 01:52:51 +0900

    chore: project init

---
+++ src/main/java/com/codeit/monew/P1MonewApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.monew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1MonewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P1MonewApplication.class, args);
+    }
+
+}
