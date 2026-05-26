4073398fd94f12ef8dc1a92e90fece0063b8f3f7
apple <apple@MacBook-Pro.local>
2026-05-26 20:26:39 +0900

    refactor: CommentController 댓글 목록 조회 파라미터 수정 및 limit 최소값 검증 추가(#23)

--- src/main/java/com/codeit/monew/domain/comment/controller/CommentController.java
+++ src/main/java/com/codeit/monew/domain/comment/controller/CommentController.java
@@ -1,11 +1,14 @@
 package com.codeit.monew.domain.comment.controller;
 
 import com.codeit.monew.domain.comment.dto.CommentDto;
+import com.codeit.monew.domain.comment.dto.CommentOrderBy;
 import com.codeit.monew.domain.comment.dto.CommentRegisterRequest;
 import com.codeit.monew.domain.comment.dto.CursorPageResponseCommentDto;
+import com.codeit.monew.domain.comment.dto.SortDirection;
 import com.codeit.monew.domain.comment.service.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -41,8 +45,11 @@ public class CommentController {
       @RequestParam UUID articleId,
       @RequestParam(required = false) String cursor,
       @RequestParam(required = false)LocalDateTime after,
-      @RequestParam(defaultValue = "50") int size
+      @RequestParam @Min(1) int limit,
+      @RequestParam CommentOrderBy orderBy,
+      @RequestParam SortDirection direction,
+      @RequestHeader("Monew-Request-User-ID") UUID requestUserId
   ) {
-    return commentService.getComments(articleId, cursor, after, size);
+    return commentService.getComments(articleId, cursor, after, limit, orderBy, direction, requestUserId);
   }
 }
