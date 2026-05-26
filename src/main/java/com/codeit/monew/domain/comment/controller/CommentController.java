872d59f804ab5fd8c69e75007467ba3648f5ef6b
apple <apple@MacBook-Pro.local>
2026-05-26 13:42:32 +0900

    refactor: ResponseEntity 제거 및 @ResponseStatus로 응답 코드 처리(#12)

--- src/main/java/com/codeit/monew/domain/comment/controller/CommentController.java
+++ src/main/java/com/codeit/monew/domain/comment/controller/CommentController.java
@@ -7,10 +7,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,11 +21,11 @@ public class CommentController {
   private final CommentService commentService;
 
   @Operation(summary = "댓글 등록", description = "댓글을 등록합니다.")
+  @ResponseStatus(HttpStatus.CREATED)
   @PostMapping
-  public ResponseEntity<CommentDto> createComment(
+  public CommentDto createComment(
       @Valid @RequestBody CommentRegisterRequest request
   ) {
-    return ResponseEntity.status(HttpStatus.CREATED)
-        .body(commentService.createComment(request));
+    return commentService.createComment(request);
   }
 }
