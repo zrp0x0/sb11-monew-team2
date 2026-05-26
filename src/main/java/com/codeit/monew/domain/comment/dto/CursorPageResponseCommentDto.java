9ee6c529269765c6fbb75da1318dcd3bc69d6ec9
apple <apple@MacBook-Pro.local>
2026-05-26 15:22:28 +0900

    feat: CursorPageResponseCommentDto 추가(#23)

---
+++ src/main/java/com/codeit/monew/domain/comment/dto/CursorPageResponseCommentDto.java
@@ -0,0 +1,29 @@
+package com.codeit.monew.domain.comment.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record CursorPageResponseCommentDto(
+    List<CommentDto> content,
+    String nextCursor,
+    LocalDateTime nextAfter,
+    int size,
+    long totalElements,
+    boolean hasNext
+) {
+  public static CursorPageResponseCommentDto of(
+      List<CommentDto> content,
+      int size,
+      long totalElements
+  ) {
+    boolean hasNext = content.size() > size;
+    List<CommentDto> result = hasNext ? content.subList(0, size) : content;
+
+    String nextCursor = hasNext ? result.get(result.size() - 1).id().toString() : null;
+    LocalDateTime nextAfter = hasNext ? result.get(result.size() - 1).createdAt() : null;
+
+    return new CursorPageResponseCommentDto(
+        result, nextCursor, nextAfter, result.size(), totalElements, hasNext
+    );
+  }
+}
