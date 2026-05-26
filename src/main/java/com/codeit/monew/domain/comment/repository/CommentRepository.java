2c63e11963f3e5ca75fef945e2cd4f5b6be5ffeb
apple <apple@MacBook-Pro.local>
2026-05-26 15:43:07 +0900

    feat: CommentRepository 커서 기반 조회 쿼리 추가(#23)

--- src/main/java/com/codeit/monew/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/codeit/monew/domain/comment/repository/CommentRepository.java
@@ -1,9 +1,39 @@
 package com.codeit.monew.domain.comment.repository;
 
 import com.codeit.monew.domain.comment.entity.Comment;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
 
+  @Query("""
+      SELECT c FROM Comment c
+      JOIN FETCH c.user
+      WHERE c.article.id = :articleId
+      ORDER BY c.createdAt DESC, c.id DESC 
+      LIMIT :limit
+      """)
+  List<Comment> findByArticleIdFirstPage (@Param("articleId") UUID articleId, @Param("limit") int limit);
+
+  @Query("""
+      SELECT c FROM Comment c
+      JOIN FETCH c.user
+      WHERE c.article.id = :articleId
+        AND (c.createdAt < :after
+          OR (c.createdAt = : after AND c.id < :cusorId))
+      ORDER BY c.createdAt DESC, c.id DESC 
+      LIMIT :limit
+      """)
+  List<Comment> findByArticleIdAfterCursor (
+      @Param("articleId") UUID articleId,
+      @Param("after")LocalDateTime after,
+      @Param("cursorId") UUID cursorId,
+      @Param("limit") int limit
+  );
+
+  long countByArticleId(UUID articleId);
 }
