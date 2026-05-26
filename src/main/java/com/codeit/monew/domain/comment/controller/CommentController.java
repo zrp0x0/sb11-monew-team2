1023f5f4622243db9763f242faae14589a069f53
apple <apple@MacBook-Pro.local>
2026-05-26 17:42:44 +0900

    feat: 댓글 목록 조회 API 엔드포인트 추가(#23)

--- src/main/java/com/codeit/monew/domain/comment/controller/CommentController.java
+++ src/main/java/com/codeit/monew/domain/comment/controller/CommentController.java
@@ -2,14 +2,19 @@ package com.codeit.monew.domain.comment.controller;
 
 import com.codeit.monew.domain.comment.dto.CommentDto;
 import com.codeit.monew.domain.comment.dto.CommentRegisterRequest;
+import com.codeit.monew.domain.comment.dto.CursorPageResponseCommentDto;
 import com.codeit.monew.domain.comment.service.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +32,17 @@ public class CommentController {
       @Valid @RequestBody CommentRegisterRequest request
   ) {
     return commentService.createComment(request);
+  }
+
+  @Operation(summary = "댓글 목록 조회", description = "댓글 목록을 조회합니다.")
+  @ResponseStatus(HttpStatus.OK)
+  @GetMapping
+  public CursorPageResponseCommentDto getComments(
+      @RequestParam UUID articleId,
+      @RequestParam(required = false) String cursor,
+      @RequestParam(required = false)LocalDateTime after,
+      @RequestParam(defaultValue = "50") int size
+  ) {
+    return commentService.getComments(articleId, cursor, after, size);
   }
 }
