a455126b7bebd96115153a300fd61d5d3f49bb9d
apple <apple@MacBook-Pro.local>
2026-05-27 18:13:52 +0900

    fix: updateComment에 @Transactional 추가(#33)

--- src/main/java/com/codeit/monew/domain/comment/service/CommentService.java
+++ src/main/java/com/codeit/monew/domain/comment/service/CommentService.java
@@ -111,6 +111,7 @@ public class CommentService {
     return CursorPageResponseCommentDto.of(dtos, limit, totalElements, orderBy);
   }
 
+  @Transactional
   public CommentDto updateComment(UUID commentId, UUID userId, CommentUpdateRequest request) {
     Comment comment = commentRepository.findById(commentId)
         .orElseThrow(() -> new CommentException(CommentErrorCode.COMMENT_NOT_FOUND));
