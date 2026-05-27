14f5e91c090568917a3978fecde9e10191fd856f
성결 <ow00us@gmail.com>
2026-05-27 09:20:30 +0900

    refactor: 로그인 리퀘스트 필드 값 어노테이션 추가(#21)

--- src/main/java/com/codeit/monew/domain/user/dto/request/UserLoginRequest.java
+++ src/main/java/com/codeit/monew/domain/user/dto/request/UserLoginRequest.java
@@ -3,6 +3,7 @@ package com.codeit.monew.domain.user.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Schema(description = "로그인 정보")
 public record UserLoginRequest(
@@ -13,6 +14,7 @@ public record UserLoginRequest(
 
         @Schema(description = "로그인 비밀번호")
         @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+        @Size(min = 6, max = 20, message = "비밀번호는 6자 이상 20자 이하여야 합니다.")
         String password
 ) {
 
