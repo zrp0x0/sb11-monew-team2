ed2fe8261f9ae976ddd32d90c145afd212de6600
apple <apple@MacBook-Pro.local>
2026-05-27 11:48:50 +0900

    feat: PATCH /api/comments/{commentId} 엔드포인트 추가(#33)

--- src/main/java/com/codeit/monew/domain/comment/controller/CommentController.java
+++ src/main/java/com/codeit/monew/domain/comment/controller/CommentController.java
@@ -3,6 +3,7 @@ package com.codeit.monew.domain.comment.controller;
 import com.codeit.monew.domain.comment.dto.CommentDto;
 import com.codeit.monew.domain.comment.dto.CommentOrderBy;
 import com.codeit.monew.domain.comment.dto.CommentRegisterRequest;
+import com.codeit.monew.domain.comment.dto.CommentUpdateRequest;
 import com.codeit.monew.domain.comment.dto.CursorPageResponseCommentDto;
 import com.codeit.monew.domain.comment.dto.SortDirection;
 import com.codeit.monew.domain.comment.service.CommentService;
@@ -14,6 +15,8 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -51,5 +54,16 @@ public class CommentController {
       @RequestHeader("Monew-Request-User-ID") UUID requestUserId
   ) {
     return commentService.getComments(articleId, cursor, after, limit, orderBy, direction, requestUserId);
+  }
+
+  @Operation(summary = "댓글 정보 수정", description = "댓글의 내용을 수정합니다.")
+  @ResponseStatus(HttpStatus.OK)
+  @PatchMapping("/{commentId}")
+  public CommentDto updateComment(
+      @PathVariable UUID commentId,
+      @RequestHeader("Monew-Request-User-ID") UUID requestUserId,
+      @Valid @RequestBody CommentUpdateRequest request
+  ) {
+    return commentService.updateComment(commentId, requestUserId, request);
   }
 }
