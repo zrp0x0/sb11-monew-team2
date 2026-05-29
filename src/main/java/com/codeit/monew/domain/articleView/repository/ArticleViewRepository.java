8bd7a73e038756ed8d4f1dc95d51bc4b60272bbb
성결 <ow00us@gmail.com>
2026-05-29 09:45:00 +0900

    fix: JPQL 추가(#43)

--- src/main/java/com/codeit/monew/domain/articleView/repository/ArticleViewRepository.java
+++ src/main/java/com/codeit/monew/domain/articleView/repository/ArticleViewRepository.java
@@ -4,8 +4,19 @@ import com.codeit.monew.domain.articleView.entity.ArticleView;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ArticleViewRepository extends JpaRepository<ArticleView, UUID> {
 
-    Optional<ArticleView> findByUserIdAndArticleId(UUID userId, UUID articleId);
+    @Query("""
+            SELECT av
+            FROM ArticleView av
+            WHERE av.user.id = :userId
+              AND av.article.id = :articleId
+            """)
+    Optional<ArticleView> findByUserIdAndArticleId(
+            @Param("userId") UUID userId,
+            @Param("articleId") UUID articleId
+    );
 }
