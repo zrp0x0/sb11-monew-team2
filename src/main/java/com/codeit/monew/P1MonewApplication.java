367d437b58c527a68b19abbe8833cf6c1bf3d5a9
taehk23 <terrypotterk@gmail.com>
2026-06-01 16:14:17 +0900

    chore: openfeign 및 Naver API 기본 설정

--- src/main/java/com/codeit/monew/P1MonewApplication.java
+++ src/main/java/com/codeit/monew/P1MonewApplication.java
@@ -2,7 +2,11 @@ package com.codeit.monew;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableFeignClients
+@EnableScheduling
 @SpringBootApplication
 public class P1MonewApplication {
 
