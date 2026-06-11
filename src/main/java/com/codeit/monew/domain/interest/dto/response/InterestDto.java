ce220a3fe2e107dc73fe4a881345ffe1b2364a87
Seongmin Kang <seongmin0244@hanyang.ac.kr>
2026-06-11 10:06:25 +0900

    refactor: DTO 이름 통일 및 subscription_not_found 에러 코드 추가

--- src/main/java/com/codeit/monew/domain/interest/dto/response/InterestDto.java
+++ src/main/java/com/codeit/monew/domain/interest/dto/response/InterestDto.java
@@ -4,7 +4,7 @@ import com.codeit.monew.domain.interest.entity.Interest;
 import java.util.List;
 import java.util.UUID;
 
-public record InterestResponse(
+public record InterestDto(
     UUID id,
     String name,
     List<String> keywords,
@@ -13,8 +13,8 @@ public record InterestResponse(
 ) {
 
   // TODO: 요청자 ID가 들어오지 않을 시 -> subscribedByMe = false로 지정
-  public static InterestResponse from(Interest interest) {
-    return new InterestResponse(
+  public static InterestDto from(Interest interest) {
+    return new InterestDto(
         interest.getId(),
         interest.getName(),
         interest.getKeywords(),
@@ -22,8 +22,8 @@ public record InterestResponse(
         false);
   }
 
-  public static InterestResponse of(Interest interest, boolean subscribedByMe) {
-    return new InterestResponse(
+  public static InterestDto of(Interest interest, boolean subscribedByMe) {
+    return new InterestDto(
         interest.getId(),
         interest.getName(),
         interest.getKeywords(),
