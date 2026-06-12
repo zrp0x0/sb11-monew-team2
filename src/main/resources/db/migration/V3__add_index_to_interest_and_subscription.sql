4f26bbd5be7b68864e94e50c398910d17803eb03
seongmin0244 <seongmin0244@hanyang.ac.kr>
2026-06-12 15:57:40 +0900

    Update V3__add_index_to_interest_and_subscription.sql

--- src/main/resources/db/migration/V3__add_index_to_interest_and_subscription.sql
+++ src/main/resources/db/migration/V3__add_index_to_interest_and_subscription.sql
@@ -1,2 +1,6 @@
-CREATE INDEX idx_article_interests_interest_id ON article_interests (interest_id);
-CREATE INDEX idx_subscriptions_user_id ON subscriptions (user_id);
+CREATE INDEX IF NOT EXISTS idx_article_interests_interest_id
+  ON article_interests (interest_id);
+
+CREATE INDEX IF NOT EXISTS idx_subscriptions_user_id
+  ON subscriptions (user_id);
+
